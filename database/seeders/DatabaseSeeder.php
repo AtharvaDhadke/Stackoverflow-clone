2bbc44e098e87a1165956065cccb90b84a3f32c7
Atharva Dhadke <atharvadhadke2002@gmail.com>
2021-07-05 18:59:36 +0530

    Created Migration for Question and added dummy data into database with seeder

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Question;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory(5)->create()->each(function(User $user){
+            for($i = 1; $i < rand(5,10); $i++) {
+                $user->questions()->create(Question::factory()->make()->toArray());
+            }
+        });
+    }
+}
